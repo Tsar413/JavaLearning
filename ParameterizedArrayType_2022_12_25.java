6102de762a038102beed3459662aa34b927a9119
Tsar413 <96027469+Tsar413@users.noreply.github.com>
2022-12-25 13:53:05 +0000

    Add files via upload

---
+++ ParameterizedArrayType_2022_12_25.java
@@ -0,0 +1,24 @@
+package com.company.javaRelearn;
+
+class ClassParameter<T> {
+    public T[] f(T[] arg) {
+        return arg;
+    }
+}
+
+class MethodParameter{
+    public static <T> T[] f(T[] arg) {
+        return arg;
+    }
+}
+
+public class ParameterizedArrayType_2022_12_25 {
+    public static void main(String[] args) {
+        Integer[] ints = {1,2,3,4,5};
+        Double[] doubles = {1.1,2.2,3.3,4.4,5.5};
+        Integer[] ints2 = new ClassParameter<Integer>().f(ints);
+        Double[] doubles2 = new ClassParameter<Double>().f(doubles);
+        ints2 = MethodParameter.f(ints);
+        doubles2 = MethodParameter.f(doubles);
+    }
+}
