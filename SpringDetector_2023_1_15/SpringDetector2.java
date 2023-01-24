65047dbb1955736b7adfe020ee5d2b94ceb69c03
Tsar413 <96027469+Tsar413@users.noreply.github.com>
2023-01-24 12:42:46 +0000

    Add files via upload

---
+++ SpringDetector_2023_1_15/SpringDetector2.java
@@ -0,0 +1,7 @@
+package com.company.javaRelearn.SpringDetector_2023_1_15;
+
+public class SpringDetector2 {
+    public static void main(String[] args) throws Exception {
+        SpringDetector.detectSpring(GroundDog2.class);
+    }
+}
