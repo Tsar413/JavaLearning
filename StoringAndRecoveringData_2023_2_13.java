442d6dc2345a4972711a0de435b86b753f7717b8
Tsar413 <96027469+Tsar413@users.noreply.github.com>
2023-02-13 23:51:21 +0000

    Add files via upload

---
+++ StoringAndRecoveringData_2023_2_13.java
@@ -0,0 +1,23 @@
+package com.company.javaRelearn;
+
+import java.io.*;
+
+public class StoringAndRecoveringData_2023_2_13 {
+    public static void main(String[] args) throws IOException {
+        DataOutputStream out = new DataOutputStream(
+                new BufferedOutputStream(
+                        new FileOutputStream("Data.txt")));
+        out.writeDouble(3.14159);
+        out.writeUTF("That was pi");
+        out.writeDouble(1.41413);
+        out.writeUTF("Square root of 2");
+        out.close();
+        DataInputStream in = new DataInputStream(
+                new BufferedInputStream(
+                        new FileInputStream("Data.txt")));
+        System.out.println(in.readDouble());
+        System.out.println(in.readUTF());
+        System.out.println(in.readDouble());
+        System.out.println(in.readUTF());
+    }
+}
