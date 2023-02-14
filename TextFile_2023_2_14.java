b0d67e7a3c912f8ab47d8337ab877a07960408d4
Tsar413 <96027469+Tsar413@users.noreply.github.com>
2023-02-14 13:40:08 +0000

    Add files via upload

---
+++ TextFile_2023_2_14.java
@@ -0,0 +1,65 @@
+package com.company.javaRelearn;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class TextFile_2023_2_14 extends ArrayList<String> {
+    public static String read(String fileName) {
+        StringBuilder sb = new StringBuilder();
+        try {
+            BufferedReader in = new BufferedReader(new FileReader(
+                    new File(fileName).getAbsoluteFile()));
+            try {
+                String s;
+                while ((s = in.readLine()) != null) {
+                    sb.append(s);
+                    sb.append("\n");
+                }
+            } finally {
+                in.close();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return sb.toString();
+    }
+
+    public static void write(String fileName, String text) {
+        try {
+            PrintWriter out = new PrintWriter(
+                    new File(fileName).getAbsoluteFile());
+            try {
+                out.print(text);
+            } finally {
+                out.close();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public TextFile_2023_2_14(String fileName, String splitter) {
+        super(Arrays.asList(read(fileName).split(splitter)));
+        if (get(0).equals("")) remove(0);
+    }
+
+    public TextFile_2023_2_14(String fileName) {
+        this(fileName, "\n");
+    }
+
+    public void write(String fileName) {
+        try {
+            PrintWriter out = new PrintWriter(
+                    new File(fileName).getAbsoluteFile());
+            try {
+                for (String item : this)
+                    out.println(item);
+            } finally {
+                out.close();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
