e5ad241665d73297b86775fccf4863c9adc97434
Tsar413 <96027469+Tsar413@users.noreply.github.com>
2023-02-19 13:59:05 +0000

    Add files via upload

---
+++ GetChannel_2023_2_19.java
@@ -0,0 +1,28 @@
+package com.company.javaRelearn;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
+public class GetChannel_2023_2_19 {
+    private static final int BSIZE = 1024;
+
+    public static void main(String[] args) throws Exception {
+        FileChannel fc = new FileOutputStream("data.txt").getChannel();
+        fc.write(ByteBuffer.wrap("Some text ".getBytes()));
+        fc.close();
+        fc = new RandomAccessFile("data.txt","rw").getChannel();
+        fc.position(fc.size());
+        fc.write(ByteBuffer.wrap("Some more ".getBytes()));
+        fc.close();
+        fc = new FileInputStream("data.txt").getChannel();
+        ByteBuffer buff = ByteBuffer.allocate(BSIZE);
+        fc.read(buff);
+        buff.flip();
+        while (buff.hasRemaining()) {
+            System.out.print((char) buff.get());
+        }
+    }
+}
