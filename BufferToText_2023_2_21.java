52f00002d094b2c23335be75c85876c1b571a754
Tsar413 <96027469+Tsar413@users.noreply.github.com>
2023-02-21 12:51:32 +0000

    Add files via upload

---
+++ BufferToText_2023_2_21.java
@@ -0,0 +1,44 @@
+package com.company.javaRelearn;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.charset.Charset;
+
+public class BufferToText_2023_2_21 {
+    private static final int BSIZE = 1024;
+
+    public static void main(String[] args) throws Exception {
+        FileChannel fc = new FileOutputStream("data.txt").getChannel();
+        fc.write(ByteBuffer.wrap("Some text".getBytes()));
+        fc.close();
+        fc = new FileInputStream("data.txt").getChannel();
+        ByteBuffer buff = ByteBuffer.allocate(BSIZE);
+        fc.read(buff);
+        buff.flip();
+        System.out.println(buff.asCharBuffer());
+        buff.rewind();
+        String encoding = System.getProperty("file.encoding");
+        System.out.println("Decoding using " + encoding + ": " +
+                Charset.forName(encoding).decode(buff));
+        fc = new FileOutputStream("data2.txt").getChannel();
+        fc.write(ByteBuffer.wrap("Some text".getBytes("UTF-16BE")));
+        fc.close();
+        fc = new FileInputStream("data2.txt").getChannel();
+        buff.clear();
+        fc.read(buff);
+        buff.flip();
+        System.out.println(buff.asCharBuffer());
+        fc = new FileOutputStream("data2.txt").getChannel();
+        buff = ByteBuffer.allocate(24);
+        buff.asCharBuffer().put("Some text");
+        fc.write(buff);
+        fc.close();
+        fc = new FileInputStream("data2.txt").getChannel();
+        buff.clear();
+        fc.read(buff);
+        buff.flip();
+        System.out.println(buff.asCharBuffer());
+    }
+}
