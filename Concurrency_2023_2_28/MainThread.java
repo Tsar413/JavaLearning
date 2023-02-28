b2a4fe04530f96a9d0e36f5f16f1c9143768d039
Tsar413 <96027469+Tsar413@users.noreply.github.com>
2023-02-28 12:49:45 +0000

    Add files via upload

---
+++ Concurrency_2023_2_28/MainThread.java
@@ -0,0 +1,8 @@
+package com.company.javaRelearn.Concurrency_2023_2_28;
+
+public class MainThread {
+    public static void main(String[] args) {
+        LiftOff liftOff = new LiftOff();
+        liftOff.run();
+    }
+}
