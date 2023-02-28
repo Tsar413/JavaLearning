bc72e811eaae61a63a2fe5b1c4c94de6163df491
Tsar413 <96027469+Tsar413@users.noreply.github.com>
2023-02-28 21:36:52 +0800

    Delete Concurrency_2023_2_28 directory

--- Concurrency_2023_2_28/MainThread.java
+++
@@ -1,8 +0,0 @@
-package com.company.javaRelearn.Concurrency_2023_2_28;
-
-public class MainThread {
-    public static void main(String[] args) {
-        LiftOff liftOff = new LiftOff();
-        liftOff.run();
-    }
-}
